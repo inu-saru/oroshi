7fa39639001d1c3d97d43b4fea26509b3ac4b14c
inu_saru <inu_saru_eagle@yahoo.co.jp>
2024-07-17 23:54:01 +0700

    c5af444d feat: instal spacy

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
-FROM python:3.9.10-alpine3.14
+FROM python:3.12.4-slim
 WORKDIR /usr/src/app
 ENV FLASK_APP=app
 COPY /app/requirements.txt ./
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
+RUN pip install -U pip setuptools wheel
+RUN pip install -U spacy
+RUN python -m spacy download en_core_web_sm
--- app/app.py
+++ app/app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+import spacy
+import en_core_web_sm
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hello, world"
+    nlp = spacy.load("en_core_web_sm")
+    nlp = en_core_web_sm.load()
+    doc = nlp("This is a sentence.")
+    text = [(w.text, w.pos_) for w in doc]
+    return text
