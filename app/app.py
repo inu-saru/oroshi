e35494e173f8ec64e27dabdacd1e1d28eaad1f97
inu_saru <inu_saru_eagle@yahoo.co.jp>
2024-10-12 14:06:25 +0700

    1170e64e add: language ja

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from googletrans import Translator
 import spacy
 import en_core_web_sm
+import ja_core_news_sm
+from lingua import Language, LanguageDetectorBuilder
 
 app = Flask(__name__)
     
@@ -18,33 +20,24 @@ def translate():
 def translate_relations():
     params = request.get_json()
     q = params['q']
+    language_type = detect_language_type(q)
     targets = params['targets']
     exclude_poses = params['exclude_poses']
-    language_type = 'en'
 
-    nlp = spacy.load("en_core_web_sm")
-    # TODO: language_typeを元に多言語対応する
-    nlp = en_core_web_sm.load()
+    nlp = get_nlp(language_type)
     doc = nlp(q)
-    
-    dicts = []
-    for i, w in enumerate(doc):
-        if w.pos_ in exclude_poses:
-            continue
-
-        vocabulary = translate_dict(w.text.lower(), targets)
-        if len(vocabulary) == 0:
-            continue
-        
-        dict = { 'vocabulary': vocabulary, 'relationship': { 'positions': [i], 'language_type': language_type }, 'meta': { 'pos': w.pos_ } }
-        dicts.append(dict)
-
-    return jsonify(dicts)
+    translates = get_translates(language_type, doc, targets, exclude_poses)
+    return jsonify(translates)
 
 def translate_dict(q, targets):
     dict = {}
     for target in targets:
         dict.update(g_translate(q, target))
+    restponse_translated_keys = list(dict.keys())
+    unnecessary_keys = [i for i in restponse_translated_keys if i not in targets]
+    for key in unnecessary_keys:
+        dict.pop(key)
+
     return dict
 
 def g_translate(q, target):
@@ -55,3 +48,42 @@ def g_translate(q, target):
     except Exception as e:
         # print(e, flush=True)
         return {}
+    
+def detect_language_type(strings):
+    detector = build_detector()
+    type = detector.detect_language_of(strings)
+    dict = get_language_dict()
+    return dict[type]
+
+def build_detector():
+    languages = [Language.ENGLISH, Language.JAPANESE, Language.THAI]
+    return LanguageDetectorBuilder.from_languages(*languages).build()
+
+def get_language_dict():
+    return { Language.ENGLISH: 'en', Language.JAPANESE: 'ja', Language.THAI: 'th' }
+
+def get_nlp(language_type):
+    if language_type == 'en':
+        nlp = spacy.load("en_core_web_sm")
+        nlp = en_core_web_sm.load()
+        return nlp
+    elif language_type == 'ja':
+        nlp = spacy.load("ja_core_news_sm")
+        nlp = ja_core_news_sm.load()
+    else:
+        nlp = None
+    return nlp
+
+def get_translates(language_type, doc, targets, exclude_poses):
+    dicts = []
+    for i, w in enumerate(doc):
+        if w.pos_ in exclude_poses:
+            continue
+
+        vocabulary = translate_dict(w.lemma_, targets)
+        if len(vocabulary) == 0:
+            continue
+        
+        dict = { 'vocabulary': vocabulary, 'relationship': { 'positions': [i], 'language_type': language_type }, 'meta': { 'pos': w.pos_ } }
+        dicts.append(dict)
+    return dicts
--- app/test_app.py
+++ app/test_app.py
@@ -1,7 +1,7 @@
 from app import app
 
 def test_index_route():
-    params = dict(q = 'We must respect the will of the individual.', targets = ['ja', 'th'])
+    params = dict(q = 'We must respect the will of the individual.', targets = ['en', 'ja', 'th'])
     response = app.test_client().post('/translate', json=params)
     json = response.get_json()
 
@@ -10,10 +10,10 @@ def test_index_route():
     assert json['ja'] == '私たちは個人の意志を尊重しなければなりません。'
     assert json['th'] == 'เราต้องเคารพเจตจำนงของแต่ละบุคคล'
 
-def test_relations_route():
+def test_relations_route_en():
     params = dict(
         q = 'We must respect the will of the individual.',
-        targets = ['ja', 'th'],
+        targets = ['en', 'ja', 'th'],
         exclude_poses = ['ADP', 'AUX', 'DET', 'PRON', 'PUNCT'])
     response = app.test_client().post('/translate_relations', json=params)
     json = response.get_json()
@@ -22,3 +22,17 @@ def test_relations_route():
     assert json[0] == {'meta': {'pos': 'VERB'}, 'relationship': {'language_type': 'en', 'positions': [2]}, 'vocabulary': {'en': 'respect', 'ja': '尊敬', 'th': 'เคารพ'}}
     assert json[1] == {'meta': {'pos': 'NOUN'}, 'relationship': {'language_type': 'en', 'positions': [4]}, 'vocabulary': {'en': 'will', 'ja': '意思', 'th': 'จะ'}}
     assert json[2] == {'meta': {'pos': 'NOUN'}, 'relationship': {'language_type': 'en', 'positions': [7]}, 'vocabulary': {'en': 'individual', 'ja': '個人', 'th': 'รายบุคคล'}}
+
+def test_relations_route_ja():
+    params = dict(
+        q = '個人の意志は尊重しなければいけない',
+        targets = ['en', 'ja', 'th'],
+        exclude_poses = ['ADP', 'AUX', 'DET', 'PRON', 'PUNCT', 'SCONJ'])
+    response = app.test_client().post('/translate_relations', json=params)
+    json = response.get_json()
+
+    assert response.status_code == 200
+    assert json[0] == {'meta': {'pos': 'NOUN'}, 'relationship': {'language_type': 'ja', 'positions': [0]}, 'vocabulary': {'en': 'personal', 'ja': '個人的', 'th': 'ส่วนตัว'}}
+    assert json[1] == {'meta': {'pos': 'NOUN'}, 'relationship': {'language_type': 'ja', 'positions': [2]}, 'vocabulary': {'en': 'will', 'ja': '意思', 'th': 'จะ'}}
+    assert json[2] == {'meta': {'pos': 'VERB'}, 'relationship': {'language_type': 'ja', 'positions': [4]}, 'vocabulary': {'en': 'respect', 'ja': '尊敬', 'th': 'เคารพ'}}
+    assert json[3] == {'meta': {'pos': 'VERB'}, 'relationship': {'language_type': 'ja', 'positions': [8]}, 'vocabulary': {'en': 'Be', 'ja': 'いける', 'th': 'ไป'}}
